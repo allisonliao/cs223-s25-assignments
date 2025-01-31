9fd5f529fcd91216458f520d064feaef180d1a01
Allison Liao <allison.y.liao@gmail.com>
2025-01-31 16:37:03 -0500

    Add password.c

--- A02/password.c
+++ A02/password.c
@@ -1,9 +1,33 @@
 /***************************************************
+ * password.c
  * Author: 
- * 
+ * Creates a bad password from input.
  */
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+  printf("Enter a word: ");
+  char input[100];
+  scanf("%s", input);
+
+  char curr;
+
+  for (int i = 0; i < strlen(input); i++) {
+    curr = input[i];
+    if (curr == 97) {
+      // character is 'a'
+      input[i] = '@';
+    } else if (curr == 101) {
+      // character is 'e'
+      input[i] = '3';
+    } else if (curr == 108) {
+      // character is 'l'
+      input[i] = '1';
+    }
+  }
+
+  printf("Your bad password is %s\n", input);
+
   return 0;
 }
