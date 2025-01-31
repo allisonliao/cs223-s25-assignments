a711aa1cee19e3df92499cb7746ea7ca58518d7f
Allison Liao <allison.y.liao@gmail.com>
2025-01-31 16:37:57 -0500

    Add cypher.c

--- A02/cypher.c
+++ A02/cypher.c
@@ -1,9 +1,35 @@
 /***************************************************
+ * cypher.c
  * Author: 
- * 
+ * Implements a shift cypher, given a shift.
  */
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+
+  printf("Enter a word: ");
+  char buffer[64];
+  char word[100];
+  scanf("%s", word);
+  strcpy(buffer, word);
+  
+  printf("Enter a shift: ");
+  int shift;
+  scanf("%d", &shift);
+
+  char curr;
+
+  for (int i = 0; i < strlen(buffer); i++) {
+    curr = buffer[i];
+    if (curr > 122 -shift) {
+      buffer[i] = curr - 26 + shift;
+    } else {
+      buffer[i] = curr + shift;
+    }
+  }
+
+  printf("Your cypher is: %s\n", buffer);
+
   return 0;
 }
