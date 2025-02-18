b6eeb2f381caef9c8586bc09bb4361e224c67c47
Allison Liao <allison.y.liao@gmail.com>
2025-02-18 15:31:05 -0500

    J02 add strblock.c

--- J02/strblock.c
+++ J02/strblock.c
@@ -1,4 +1,40 @@
-int main()
-{
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+  printf("Enter a string: ");
+  char input[32];
+  scanf("%s", input);
+
+  int length = strlen(input);
+  // allocate empty array
+  char* array = malloc(sizeof(char) * length * length);
+  memset(array, ' ', length * length);
+
+  for (int i = 0; i < length; i++) {
+    // first row
+    array[i] = input[i];
+
+    // last row
+    array[(length - 1) * length + i] = input[i];
+
+    // diagonal
+    array[i * length + i] = input[i];
+  }
+
+  for (int i = 0; i < length; i++) {
+    for (int j = 0; j < length; j++) {
+      //if (array[i*length + j] != NULL) {
+        printf("%c ", array[i * length + j]);
+      //} else {
+      //  printf("  ");
+      //}
+    }
+    printf("\n");
+  }
+
+  free(array);
+
   return 0;
 }
