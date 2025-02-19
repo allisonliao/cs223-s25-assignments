5b5fd87ca19f710a151b63da0bd77724541bd320
Allison Liao <allison.y.liao@gmail.com>
2025-02-19 10:57:21 -0500

    HW 5 initial

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -1,6 +1,6 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
+ * Author: Allison Liao
+ * Date: 02/21/25
  * Description
  ---------------------------------------------*/
 #include <stdio.h>
@@ -11,7 +11,61 @@
 // Choose *one* to implement (do not remove the other one!)
 
 struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+  FILE *file;
+  file = fopen(filename, "r");
+  if (file == NULL) {
+    printf("Error opening file. Exiting...");
+    return NULL;
+  }
+  
+  // parse header
+  char buffer[255];
+  fgets(buffer, sizeof(buffer), file);
+
+  char *token;
+  char delims[4] = {' ', '\t', '\n', '\r'};
+  token = strtok(buffer, delims);
+  char* header[4];
+
+  for (int i = 0; token != 0; i++) {
+    header[i] = token;
+    token = strtok(NULL, delims);
+  }
+
+  // if (header[0] != "P6") {
+  //   printf("PPM is not binary. Exiting...");
+  //   return NULL;
+  // }
+
+  *w = atoi(header[1]);
+  *h = atoi(header[2]);
+
+
+  // account for single commented line, starting ith #
+
+  struct ppm_pixel* output = malloc(sizeof(struct ppm_pixel) * *w * *h);
+  if (output == NULL) {
+    printf("Memory could not be allocated. Exiting...");
+    return NULL;
+  }
+  char del[4] = {',', ' ', '(', ')'};
+  for (int i = 0; i < *h; i++) {
+    fgets(buffer, sizeof(buffer), file);
+    for (int j = 0; j < *w; j++) {
+      token = strtok(buffer, del);
+      int red = atoi(token);
+      token = strtok(buffer, del);
+      int blue = atoi(token);
+      token = strtok(buffer, del);
+      int green = atoi(token);
+      struct ppm_pixel pixel = {.red = red, .blue = blue, .green = green};
+      output[i * *h + j] = pixel;
+    }
+  }
+
+
+  fclose(file);
+  return output;
 }
 
 struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
--- A05/test_read.c
+++ A05/test_read.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
  * Author: 
  * Date: 
- * Description
+ * Tests read_ppm.c method.
  ---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +12,12 @@ int main() {
   struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
 
   // todo: print out the grid of pixels
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      struct ppm_pixel pixel = pixels[i * h + j];
+      printf("(%d, %d, %d)", pixel.red, pixel.blue, pixel.green);
+    }
+  }
   free(pixels);
   return 0;
 }
