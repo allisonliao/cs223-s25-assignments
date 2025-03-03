64ec0f3fa777b578e9f5f4f95956868c1642ab70
Allison Liao <allison.y.liao@gmail.com>
2025-03-03 15:29:00 -0500

    Commit bitmap.c

--- A07/bitmap.c
+++ A07/bitmap.c
@@ -1,15 +1,30 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Allison Liao
+ * Date: 03/07/25
+ * Takes input 64 bit unsigned long, outputs bpp.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   unsigned long img;
   scanf(" %lx", &img);
   printf("Image (unsigned long): %lx\n", img);
+  char output[64];
+  memset(output, ' ', 64);
 
-  // todo: your code here
+  for (int i = 0; i < 64; i++) {
+    unsigned long mask = 0x1UL << i;
+    if (mask & img) {
+      output[63-i] = '@';
+    }
+  }
+
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      printf("%c", output[i * 8 + j]);
+    }
+    printf("\n");
+  }
   return 0;
 }
