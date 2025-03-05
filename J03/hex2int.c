28e3b190e12751bafc412092d60045554ce50498
Allison Liao <allison.y.liao@gmail.com>
2025-03-04 19:24:35 -0500

    Code jam 3 WIP

--- J03/hex2int.c
+++ J03/hex2int.c
@@ -1,4 +1,49 @@
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+  char string[32];
+  int ind = 2;
+  while (argv[1][ind] != '\0') {
+    string[ind-2] = argv[1][ind];
+    ind++;
+  }
+  string[ind-2] = '\0'; 
+  printf("string = %s\n", string);
+
+
+  if (argv[1][1] == 'b') {
+    int num = atoi(string);
+    unsigned int mask = 0b1;
+    int result = 0;
+
+    for (int i = strlen(string)-1; i >= 0; i--) {
+      mask = 0b1 << i;
+      if (mask & num) result = result * 2 + 1;
+      else result = result * 2;
+    }
+    printf("result = %d\n", result);
+
+  }
+
+  else if (argv[1][1] == 'x') {
+    // hex input
+
+    int num = atoi(string);
+    unsigned int mask = 0xF;
+    int result = 0;
+
+    for (int i = strlen(string)-1; i >= 0; i--) {
+      mask = 0xF << i * 4;
+      unsigned int curr = mask & num;
+      curr = curr >> i*4;
+      printf("curr = %d", curr);
+      result = result *16 + curr;
+    }
+    printf("result = %d\n", result);
+
+
+  }
   return 0;
 }
--- J03/int2hex.c
+++ J03/int2hex.c
@@ -1,4 +1,37 @@
-int main()
-{
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+  //printf("%s\n", argv[1]);
+  int input = atoi(argv[1]);
+  //printf("%d\n", input);
+
+  printf("0x ");
+  unsigned int mask = 0xF;
+  for (int i = 7; i >= 0; i--) {
+    mask = 0xF << i*4;
+    unsigned int curr = mask & input;
+    curr = curr >> i*4;
+    printf("%X", curr);
+    if ((i) % 2 == 0) {
+      printf(" ");
+    }
+  }
+
+  printf("\n");
+  printf("0b ");
+  int bmask = 0b1;
+  for (int i = 31; i >= 0; i--) {
+    bmask = 0b1 << i;
+    int curr = 0;
+    if (bmask & input) curr = 1;
+    printf("%d", curr);
+    if ((i) % 4 == 0) {
+      printf(" ");
+    }
+  }
+
+  printf("\n");
+  
   return 0;
 }
