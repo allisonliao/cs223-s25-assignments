e476789f254d51506d4ceb22870017a60cb1f271
Allison Liao <allison.y.liao@gmail.com>
2025-03-04 23:16:05 -0500

    Modify hex2int.c

--- J03/hex2int.c
+++ J03/hex2int.c
@@ -6,22 +6,23 @@ int main(int argc, char** argv) {
   char string[32];
   int ind = 2;
   while (argv[1][ind] != '\0') {
-    string[ind-2] = argv[1][ind];
+    string[ind - 2] = argv[1][ind];
     ind++;
   }
-  string[ind-2] = '\0'; 
+  string[ind - 2] = '\0';
   printf("string = %s\n", string);
-
 
   if (argv[1][1] == 'b') {
     int num = atoi(string);
     unsigned int mask = 0b1;
     int result = 0;
 
-    for (int i = strlen(string)-1; i >= 0; i--) {
+    for (int i = strlen(string) - 1; i >= 0; i--) {
       mask = 0b1 << i;
-      if (mask & num) result = result * 2 + 1;
-      else result = result * 2;
+      if (mask & num)
+        result = result * 2 + 1;
+      else
+        result = result * 2;
     }
     printf("result = %d\n", result);
 
@@ -29,21 +30,24 @@ int main(int argc, char** argv) {
 
   else if (argv[1][1] == 'x') {
     // hex input
-
-    int num = atoi(string);
-    unsigned int mask = 0xF;
     int result = 0;
 
-    for (int i = strlen(string)-1; i >= 0; i--) {
-      mask = 0xF << i * 4;
-      unsigned int curr = mask & num;
-      curr = curr >> i*4;
-      printf("curr = %d", curr);
-      result = result *16 + curr;
+    for (int i = 0; string[i] != '\0'; i++) {
+      int value = 0;
+      char ch = string[i];
+      if (ch >= '0' && ch <= '9') {
+        value = ch - '0';
+      } else if (ch >= 'A' && ch <= 'F') {
+        value = ch - 'A' + 10;
+      } else if (ch >= 'a' && ch <= 'f') {
+        value = ch - 'a' + 10;
+      } else printf("Error, incorrect he input.");
+      
+      result = (result << 4) | value;
     }
+
     printf("result = %d\n", result);
-
-
   }
+  
   return 0;
 }
