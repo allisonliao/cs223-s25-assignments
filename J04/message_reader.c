12edd6c10a3d64178ca68b2ee8e527a35919dba3
Allison Liao <allison.y.liao@gmail.com>
2025-03-25 16:00:44 -0400

    Submit J4

--- J04/message_reader.c
+++ J04/message_reader.c
@@ -1,4 +1,62 @@
-int main()
-{
+// J4 - Ally and Maryam
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct meta_data {
+  float version;
+  unsigned int year;
+  unsigned int length;
+};
+
+int main(int argc, char **argv) {
+  char *filename = argv[1];
+  FILE *file;
+  file = fopen(filename, "rb");
+  if (file == NULL) {
+    printf("Error opening file. Exiting...");
+    return 1;
+  }
+
+  char comment[64];
+
+  fgets(comment, 64, file);
+  printf("Comment: %s\n", comment);
+
+  struct meta_data *metadata = malloc(sizeof(struct meta_data));
+
+  // read float for version
+  float *v = malloc(sizeof(float));
+  fread(v, sizeof(*v), 1, file);
+  metadata->version = *v;
+  printf("Version: %f\n", metadata->version);
+
+  // read year
+  unsigned int *y = malloc(sizeof(int));
+  fread(y, sizeof(*y), 1, file);
+  metadata->year = *y;
+  printf("Year: %u\n", metadata->year);
+
+  // read length
+  unsigned int *l = malloc(sizeof(int));
+  fread(l, sizeof(*l), 1, file);
+  metadata->length = *l;
+  printf("Length: %u\n", metadata->length);
+
+  char *data = malloc(*l + 1);
+  int size = *l / 8;
+
+  fread(data, *l, size, file);
+
+  printf("Message: %s\n", data);
+
+  free(v);
+  free(y);
+  free(l);
+  free(data);
+  free(metadata);
+  fclose(file);
+
   return 0;
 }
--- J04/message_writer.c
+++ J04/message_writer.c
@@ -1,4 +1,36 @@
-int main()
-{
+// J4 - Ally and Maryam
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct meta_data {
+  float version;
+  unsigned int year;
+  unsigned int length;
+};
+
+int main(int argc, char** argv) {
+  struct meta_data* metadata = malloc(sizeof(struct meta_data));
+  metadata->version = 1;
+  metadata->year = 2025;
+  metadata->length = 11;
+  char* message = malloc(12);
+  strcpy(message, "hello world");
+
+  char* filename = argv[1];
+  FILE* output;
+  output = fopen(filename, "wb");
+
+  // add comment
+  fprintf(output, "# Comment\n");
+  int len = strlen(message);
+
+  fwrite(metadata, sizeof(struct meta_data), 1, output);
+  fwrite(message, 1, len + 1, output);
+
+  free(metadata);
+  free(message);
+  fclose(output);
   return 0;
 }
