f29a077921f0cdcd27eac880cea2691c3b170294
Allison Liao <allison.y.liao@gmail.com>
2025-03-26 10:33:28 -0400

    Submit warmup.c

--- A09/warmup.c
+++ A09/warmup.c
@@ -1,5 +1,30 @@
+/*----------------------------------------------
+ * Author: Allison Liao
+ * Date: 03/28/25
+ * Program that spawns processes according to spec.
+ ---------------------------------------------*/
+
 #include <stdio.h>
+#include <sys/_types/_pid_t.h>
+#include <unistd.h>
 
 int main() {
+  pid_t ret;
+  printf("%d] A0\n", getpid());
+  ret = fork();
+  if (ret == 0) { // child (B1)
+    printf("%d] B1\n", getpid());
+  } else { // parent (B0)
+    printf("%d] B0\n", getpid());
+    ret = fork();
+    if (ret == 0) { // child (C1)
+      printf("%d] C1\n", getpid());
+
+    } else { // parent (C0)
+      printf("%d] C0\n", getpid());
+    }
+  }
+  printf("%d] Bye\n", getpid());
+  
   return 0;
 }
